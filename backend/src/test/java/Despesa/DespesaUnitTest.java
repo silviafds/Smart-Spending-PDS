1b3de69840834f4e72a72070c64c3ad1b2ecc7cd
silviafds <silviafernandes974@gmail.com>
2024-03-27 17:29:27 -0300

    testes unitários de receita e despesa

---
+++ backend/src/test/java/Despesa/DespesaUnitTest.java
@@ -0,0 +1,102 @@
+package Despesa;
+
+import com.smartSpd.smartSpding.Apresentacao.Controller.DespesaController;
+import com.smartSpd.smartSpding.Core.DTO.DespesaDTO;
+import com.smartSpd.smartSpding.Core.Dominio.ContaInterna;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.smartSpd.smartSpding.Aplicacao.CasoUsoImpl.DespesaServiceImpl;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
+public class DespesaUnitTest {
+
+    private DespesaController despesaController;
+
+    @BeforeEach
+    public void init() {
+        DespesaServiceImpl despesaService = mock(DespesaServiceImpl.class);
+        despesaController = new DespesaController(despesaService);
+    }
+
+    @Test
+    @DisplayName("Cadastro de Despesa Test")
+    public void testCadastrarDespesa() {
+        ContaInterna contaInterna = new ContaInterna(39L, "conta 2", false);
+        DespesaDTO despesaDTO = new DespesaDTO(contaInterna, "Despesas Fixas", "Internet",
+                LocalDate.now(), 80.0, "Pix", "Banco do Brasil",
+                "Corrente/0044/12345678", "Claro", "Banco Bradesco",
+                "", "", "internet");
+
+        despesaController.register(despesaDTO);
+    }
+
+    @Test
+    @DisplayName("Buscar Despesa Test")
+    public void testBuscarDespesa() {
+        ResponseEntity<?> response = despesaController.buscarDespesa();
+        assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Deletar Despesa Test")
+    public void testDeletarDespesa() {
+        ResponseEntity<?> response = despesaController.deletarDespesa(4L);
+        assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Buscar Categoria de Despesa Test")
+    public void testBuscarCategoriaDespesa() {
+        ResponseEntity<?> response = despesaController.buscarCategoriaDespesa();
+        assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Buscar Titúlos Contábeis de Despesa Test")
+    public void testBuscarTituloContabilDespesa() {
+        ContaInterna contaInterna = new ContaInterna(39L, "conta 2", false);
+        DespesaDTO despesaDTO = new DespesaDTO(contaInterna, "Despesas Fixas", "Internet",
+                LocalDate.now(), 80.0, "Pix", "Banco Bradesco",
+                "Corrente/1258/0001258", "Claro", "Banco Bradesco",
+                "", "", "internet");
+
+        ResponseEntity<?> response = despesaController.editarDespesa(despesaDTO);
+        assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Dados de Despesa Test")
+    public void testDadosBancariosOrigemNotNull() {
+        ContaInterna contaInterna = new ContaInterna(39L, "conta 2", false);
+
+        DespesaDTO despesaDTO = new DespesaDTO(contaInterna, "Despesas Fixas", "Internet",
+                LocalDate.now(), 80.0, "Pix", "Banco do Brasil",
+                "Conta Corrente/0044/12345678", "Claro", "Banco Bradesco",
+                "", "", "internet");
+
+        assertNotNull(despesaDTO.getDadosBancariosOrigem());
+    }
+
+    @Test
+    @DisplayName("Dados nulos de Despesa Test")
+    public void testDadosBancariosOrigemIsNull() {
+        ContaInterna contaInterna = new ContaInterna(39L, "conta 2", false);
+
+        DespesaDTO despesaDTO = new DespesaDTO(contaInterna, "Despesas Fixas", "Internet",
+                LocalDate.now(), 80.0, "Pix", "Banco do Brasil",
+                null, "Claro", "Banco Bradesco",
+                "", "", "internet");
+
+        assertNull(despesaDTO.getDadosBancariosOrigem());
+    }
+
+}
---
+++ backend/src/test/java/Receita/ReceitaUnitTest.java
@@ -0,0 +1,95 @@
+package Receita;
+
+import com.smartSpd.smartSpding.Aplicacao.CasoUsoImpl.ReceitaServiceImpl;
+import com.smartSpd.smartSpding.Apresentacao.Controller.ReceitaController;
+import com.smartSpd.smartSpding.Core.DTO.ReceitaDTO;
+import com.smartSpd.smartSpding.Core.Dominio.ContaInterna;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+
+public class ReceitaUnitTest {
+    private ReceitaController receitaController;
+
+    @BeforeEach
+    public void init() {
+        ReceitaServiceImpl receitaService = mock(ReceitaServiceImpl.class);
+        receitaController = new ReceitaController(receitaService);
+    }
+
+    @Test
+    @DisplayName("Cadastro de Receita Test")
+    public void testCadastrarReceita() {
+        ContaInterna contaInterna = new ContaInterna(39L, "conta 2", false);
+        ReceitaDTO receitaDTO = new ReceitaDTO(contaInterna, "Investimento", "Lucro com moedas",
+                LocalDate.now(), 3000.0, "Corretora Ricco", "Pix", "Corretora Ricco",
+                "", "", "Banco Bradesco",
+                "Corrente/1234/95175328", "Lucro com conversão de moedas");
+
+        receitaController.register(receitaDTO);
+    }
+
+    @Test
+    @DisplayName("Buscar Receita Test")
+    public void testBuscarReceita() {
+        ResponseEntity<?> response = receitaController.buscarReceitas();
+        assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Deletar Receita Test")
+    public void testDeletarReceita() {
+        ResponseEntity<?> response = receitaController.deletarReceita(4L);
+        assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Buscar Categoria de Receita Test")
+    public void testBuscarCategoriaReceita() {
+        ResponseEntity<?> response = receitaController.buscarCategoriaReceita();
+        assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Editar Receita Test")
+    public void testEditarBuscarTituloContabilReceita() {
+        ContaInterna contaInterna = new ContaInterna(39L, "conta 2", false);
+        ReceitaDTO receitaDTO = new ReceitaDTO(contaInterna, "Investimento", "Lucro com moedas",
+                LocalDate.now(), 80.0, "Pix", "Banco Bradesco",
+                "Corrente/1258/0001258", "Claro", "Banco Bradesco",
+                "", "", "internet");
+
+        ResponseEntity<?> response = receitaController.editarReceita(receitaDTO);
+        assertNotNull(response);
+    }
+
+    @Test
+    @DisplayName("Dados de Receita Test")
+    public void testDadosBancariosOrigemNotNull() {
+        ContaInterna contaInterna = new ContaInterna(39L, "conta 2", false);
+        ReceitaDTO receitaDTO = new ReceitaDTO(contaInterna, "Investimento", "Lucro com moedas",
+                LocalDate.now(), 80.0, "Pix", "Banco Bradesco",
+                "Corrente/1258/0001258", "Claro", "Banco Bradesco",
+                "", "", "internet");
+
+        assertNotNull(receitaDTO.getDadosBancariosDestino());
+    }
+
+    @Test
+    @DisplayName("Dados nulos de Receita Test")
+    public void testDadosBancariosOrigemIsNull() {
+        ContaInterna contaInterna = new ContaInterna(39L, "conta 2", false);
+        ReceitaDTO receitaDTO = new ReceitaDTO(contaInterna, "Investimento", "Lucro com moedas",
+                LocalDate.now(), 80.0, "Pix", "Banco Bradesco",
+                "Corrente/1258/0001258", "Claro", "Banco Bradesco",
+                "", "", "internet");
+
+        assertNotNull(receitaDTO.getDadosBancariosDestino());
+    }
+}

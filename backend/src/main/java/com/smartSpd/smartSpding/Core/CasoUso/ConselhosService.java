12d3ea2515210c1126dacf40b8db4974abe62a59
silviafds <silviafernandes974@gmail.com>
2024-04-21 22:03:57 -0300

    remoção de comentários

--- backend/src/main/java/com/smartSpd/smartSpding/Core/CasoUso/ConselhosService.java
+++ backend/src/main/java/com/smartSpd/smartSpding/Core/CasoUso/ConselhosService.java
@@ -15,52 +15,52 @@ public interface ConselhosService {
 
     List<String> conselhosPorBalanco(BalancoRapidoDTO balancoRapidoDTO);
 
-    /*    BALANÇO: AMBOS
+    /*
     * Análise no qual verifica quando a despesa é maior que receita
     * ou quando a receita é maior que despesa  ou quando são iguais
     * */
     String avisarSobreExcessoDespesa(BalancoRapidoDTO balancoRapidoDTO);
 
-    /*    BALANÇO: AMBOS
+    /*
      * Análise no qual verifica quando a despesa é maior que receita
      * ou quando a receita é maior que despesa  ou quando são iguais
      * */
     String avisarSobreExcessoReceita(BalancoRapidoDTO balancoRapidoDTO);
 
-    /*    BALANÇO: AMBOS
+    /*
      * Análise no qual verifica quando a despesa é maior que receita
      * ou quando a receita é maior que despesa  ou quando são iguais
      * */
     String avisarSobreIgualdadeReceitaDespesa(BalancoRapidoDTO balancoRapidoDTO);
 
-    /*  BALANÇO: AMBOS E DESPESA
+    /*
      * Análise no qual verifica quando a despesa do mes atual é maior
      * ou menor que o mes anterior
      * */
     String compararDespesaMesAtualEAnterior(BalancoRapidoDTO balancoRapidoDTO);
 
-    /* BALANÇO: DESPESA
+    /*
      * Análise no qual verifica quando a despesa do mes atual é maior
      * ou menor que o mes anterior
      * */
     String compararReceitaMesAtualEAnterior(BalancoRapidoDTO balancoRapidoDTO);
 
-    /*  BALANÇO: DESPESA
+    /*
      * Análise no qual verifica quando a meta de despesa é atinginda
      * */
     String gerarConselhoMetaDespesa(LocalDate dataInicio, LocalDate dataFinal);
 
-    /* BALANÇO: RECEITA
+    /*
      * Análise no qual verifica quando a meta de receita é atinginda
      * */
     String gerarConselhoMetaReceita(LocalDate dataInicio, LocalDate dataFinal);
 
-    /* BALANÇO: AMBOS E DESPESA
+    /*
      * Análise no qual verifica qual despesa ele podera reduzir
      * */
     String despesaAReduzir(LocalDate dataInicio, LocalDate dataFinal);
 
-    /* BALANÇO: AMBOS E DESPESA
+    /*
      * Análise no qual verifica qual receita ele podera aumentar
      * */
     String receitasAAumentar(LocalDate dataInicio, LocalDate dataFinal);

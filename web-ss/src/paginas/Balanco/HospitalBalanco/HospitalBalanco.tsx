1de824013638ff600f40607f6250fbfc5d7279fa
silviafds <silviafernandes974@gmail.com>
2024-06-20 20:25:30 -0300

    criando pagina de balanço de hospital

--- web-ss/src/paginas/Balanco/HospitalBalanco/HospitalBalanco.tsx
+++ web-ss/src/paginas/Balanco/HospitalBalanco/HospitalBalanco.tsx
@@ -3,9 +3,41 @@ import {HeaderPadrao} from "../../../componentes/header/headerPadrao";
 import {Sidebar} from "../../../componentes/sidebar/sidebar";
 import {Ajuda} from "../../../componentes/ajuda/Ajuda";
 import {AjudaEnum} from "../../../core/ENUM/Ajuda";
+import {useForm} from "react-hook-form";
+import ModalIniciarBalancoRapido from "../LancamentoBalanco/Modal/ModalIniciarBalancoRapido";
+import ModalIniciarBalancoRapidoHospital from "./Modal/ModalIniciarBalancoRapidoHospital";
+
+interface DataIndexable {
+    [key: string]: string | Date | number | boolean | any;
+}
+
+interface Data extends DataIndexable {
+    nome: string;
+    tipoBalanco: string;
+    analise_balanco: string;
+    dataInicio: Date;
+    dataTermino: Date;
+    dataInicios: string;
+    dataTerminos: string;
+    tipoVisualizacao: string;
+    categoriaOuTituloContabil: number;
+    dashboard_check: boolean;
+}
+
+class IFormInputs {
+    id: any;
+}
 
 function HospitalBalanco() {
     const [nomeUsuario, setNomeUsuario] = useState<string>("");
+    const [modalAberto, setModalAberto] = useState<boolean>(false);
+    const [modalSalvarBalancoAberto, setSalvarBalancoAberto] = useState<boolean>(false);
+
+    const {
+        setValue,
+        formState: { errors },
+        watch,
+    } = useForm<IFormInputs>();
 
     useEffect(() => {
         const storageUser = localStorage.getItem('nomeUser');
@@ -13,6 +45,19 @@ function HospitalBalanco() {
             setNomeUsuario(storageUser);
         }
     }, []);
+
+    function handleCadastro() {
+        setSalvarBalancoAberto(true)
+    }
+
+    function handleAbrirModal() {
+        console.log(`modal aberto`)
+        setModalAberto(true);
+    }
+
+    function handleFecharModal() {
+        setModalAberto(false);
+    }
 
     return (
         <div>
@@ -28,7 +73,18 @@ function HospitalBalanco() {
                     <hr className={"my-4 mt-6 p-0 w-full border-gray-300"}/>
 
                     <div className="mt-10 w-full max-w-6xl ">
-                        <h1>tela de gerenciador de hospital</h1>
+                        <button className="bg-principal hover:bg-sky-800 text-white p-2 w-38 rounded-md"
+                                onClick={handleCadastro}>
+                            Criar Balanço
+                        </button>
+
+                        <button className="bg-principal hover:bg-sky-800 text-white p-2 w-38 rounded-md ml-4"
+                                onClick={handleAbrirModal}>
+                            Balanço Rápido
+                        </button>
+
+                        {modalAberto && <ModalIniciarBalancoRapidoHospital onClose={handleFecharModal} />}
+
                     </div>
                 </div>
             </div>
